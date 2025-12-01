7be1b874669c6dfc951f0175a32a9923847a2e55
Documenter.jl <documenter@juliadocs.github.io>
2025-12-01 20:30:28 +0000

    build based on 62a0d63

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Topics-course-in-computer-assisted-proofs-Spring-2026","page":"Topics course in computer-assisted proofs Spring 2026","title":"Topics course in computer-assisted proofs Spring 2026","text":"This repository contains the material for a graduate level topics course in computer-assisted proofs given at University of Minnesota Spring 2026.","category":"section"}]
+}
